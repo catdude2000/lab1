910f90b8b733aaff4a35361ce2a4d14db9a8c4ea
Michael Pace <michael.g.pace@gmail.com>
2021-09-02 10:21:05 -0500

    main syntax fixes, started following in class lab

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,30 +7,29 @@ class Main extends React.Component {
         super(props);
 
         this.state = {
-            title='title'
+            title:'title',
             hornedBeasts: [
-                {title: 'bm'}
+                {title: props.HornedBeast}
             ]
-        
+        }
+    }
     render() {
-        let beast = data[0]
         return (
             <ol>
                 {
-                    this.state.hornedBeasts.map((HornedBeast, beastIndex) => (
+                    this.state.hornedBeasts.map((beast, beastIndex) => (
                         <li key={beastIndex}>
                             <HornedBeast
                                 title={beast.title}
-                                image_url={beast.image_url
-                                    />
-                        />
+                                image_url={beast.image_url}/>
+                        </li>
+                        
                     ))
-                }
-            />    
-        )
-        
     }
+            </ol>    
+        )
 }
+}             
 
 // const Aname = (props) => <h1>This is a {props.title}</h1>
 
@@ -38,8 +37,8 @@ export default Main;
 
 
 
-// div >
-// <
+// 
+// <div >
 // HornedBeast title = 'Unicorn'
 // imageUrl = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
 // descript = 'A unicorn' / >
@@ -48,4 +47,4 @@ export default Main;
 // imageUrl = "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
 // descript = 'A Rhino Family' / >
 // <
-// /div>
+// /div/>
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import  Container  from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar'
 
 // class component
 class Header extends React.Component {
     render() {
         return (
             <header>
+                <navbar>
+                    <Navbar.Brand>Gallery of Horns!</Navbar.Brand>
+                </navbar>
                 <h1>This is awesome</h1>
             </header>
         )
