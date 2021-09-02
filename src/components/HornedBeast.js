fc419086a99c76aa8d9f11e14bbf30b0a38b88bd
Michael Pace <michael.g.pace@gmail.com>
2021-09-02 11:27:06 -0500

    finished 2 for realz

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -2,12 +2,26 @@ import React from 'react';
 import Card from 'react-bootstrap/Card'
 
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            favorites: 0,
+        };
+        }
+        handleCardClick=() => {
+            let currentFavorites = this.state.favorites;
+            this.setState({ favorites: currentFavorites + 1});
+        }
     render() {
         return (
-            <Card>
+            <Card
+            onClick={this.handleCardClick}
+            >
                 <Card.Body>
                     <Card.Img src={this.props.image_url}/>
                     <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>Favorites = {this.state.favorites}</Card.Text>
                     <Card.Text>{this.props.description}</Card.Text>
                 </Card.Body>
             </Card>
