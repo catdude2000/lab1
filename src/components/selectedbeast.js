f3f5dc684728b7fb63bd8a9e6288c014e7421d2d
Michael Pace <michael.g.pace@gmail.com>
2021-09-02 12:50:50 -0500

    modal shows on beast select

--- src/components/selectedbeast.js
+++ src/components/selectedbeast.js
@@ -5,15 +5,16 @@ import { Button, Card, Modal } from 'react-bootstrap';
 class SelectedBeast extends React.Component {
     render() {
         console.log(this.props)
-        // if (!beast) {
-        //     return null;
-    // }
+        let beast= this.props.beast
+        if (!beast) {
+            return null;
+    }
         return (
             <Modal show={this.props.show}
             onHide={this.props.handleClose}>
                 <Modal.Dialog>
                     <Modal.Header closeButton></Modal.Header>
-                    <Modal.Title>Test!</Modal.Title>
+                    <Modal.Title>{beast.title}</Modal.Title>
                     <Modal.Header />
                 </Modal.Dialog>
             </Modal>
